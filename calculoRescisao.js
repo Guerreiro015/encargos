f2f65e59549f75f9128d1b08e88666d804eb91d2
Guerreiro015 <antoniolucasluana@gmail.com>
2024-01-30 22:20:14 -0300

    atualizar

--- calculoRescisao.js
+++ calculoRescisao.js
@@ -36,7 +36,7 @@ function calcularEncargos() {
   ano_anterior = 12 - mes1
 
 
-  // Cálculo dos avos das férias proporcinais
+  // Cálculo dos avos das férias proporcinais//
   if (ano1 < ano2 && mes1 == mes2 && dia1 <= dia2) { meses_prop = 0 }
   else {
     if (ano1 < ano2 && mes1 == mes2 && dia1 > dia2) { meses_prop = 11 }
@@ -60,7 +60,7 @@ function calcularEncargos() {
 
                   if (ano1 == ano2 && mes1 > mes2) { meses_prop = 0 }
                   else {
-
+                    meses_prop = 0
                   }
 
                 }
@@ -104,13 +104,13 @@ function calcularEncargos() {
   let valNot = parseFloat(document.getElementById("val_not").value)
   let valHe = parseFloat(document.getElementById("val_he").value)
 
-  let faltas = parseFloat(document.getElementById("faltas").value)
+  let quant_faltas = parseFloat(document.getElementById("faltas").value)
   let valor_pensao = parseFloat(document.getElementById("pensao").value)
   let dep_ir = parseFloat(document.getElementById("depir").value)
 
 
 
-  let valordepir = dep_ir * 189.59
+  valor_dep_ir = dep_ir * 189.59
 
   for (i = 0; i <= adc.length; i++) {
     if (adicional_recebido == adc[i]) {
@@ -121,6 +121,9 @@ function calcularEncargos() {
       valor_do_adicional = valSal * 0.3
     }
   }
+
+
+   // Cálculo do INSS//
 
   let proventos = valSal + valNot + valHe + valor_do_adicional
   let salCalculo = valSal + valor_do_adicional
@@ -136,7 +139,7 @@ function calcularEncargos() {
     valor_ferias_vencidas = 0
   }
 
-  baseInss = proventos - faltas
+  baseInss = proventos - quant_faltas
 
   if (baseInss < 1412) {
     valorInss = baseInss * 7.5 / 100
@@ -158,8 +161,9 @@ function calcularEncargos() {
     valorInss = 908.85
   }
 
+  // Cálculo do Imposto de Renda//
 
-  deducao = valorInss + valor_pensao + valordepir
+  deducao = valorInss + valor_pensao + valor_dep_ir
 
   if (deducao <= 528) {
     deducao = 528
@@ -173,31 +177,29 @@ function calcularEncargos() {
   }
 
   if (baseIR < 2112) {
-    IR = 0
+    valor_IR = 0
 
   }
   else if (baseIR < 2826.66) {
-    IR = (baseIR * 7.5 / 100) - 158.4
+    valor_IR = (baseIR * 7.5 / 100) - 158.4
   }
 
   else if (baseIR < 3751.06) {
-    IR = (baseIR * 15 / 100) - 370.40
+    valor_IR = (baseIR * 15 / 100) - 370.40
   }
 
   else if (baseIR < 4664.69) {
-    IR = (baseIR * 22.5 / 100) - 651.73
+    valor_IR = (baseIR * 22.5 / 100) - 651.73
   }
 
   else if (baseIR > 4664.68) {
-    IR = (baseIR * 27.5 / 100) - 884.96
+    valor_IR = (baseIR * 27.5 / 100) - 884.96
   }
-
-
 
 
   document.getElementById("rescisao").innerHTML = "Rescisão: " + tipo_de_rescisao;
   document.getElementById("diasRes").innerHTML = "Dias p/ Cálculos....: " + dia2;
-  document.getElementById("tipo_adicional").innerHTML = adicional_recebido + ":  R$: " + valor_do_adicional.toFixed(2);
+  document.getElementById("tipoAdicional").innerHTML = adicional_recebido + ":  R$: " + valor_do_adicional.toFixed(2);
   document.getElementById("baseCalculo").innerHTML = "Salário p/ Cálculo..: " + salCalculo.toFixed(2);
   document.getElementById("valorDia").innerHTML = "Valor Diario.....: R$: " + valDia.toFixed(2);
   document.getElementById("avisoProjetado").innerHTML = "Aviso Projetado......: " + avos_avisoProjetado + " dias";
@@ -207,11 +209,9 @@ function calcularEncargos() {
   document.getElementById("avoDecimo").innerHTML = "13º Propor. ...:__" + decimo + "__Avos"
   document.getElementById("valorDecimo").innerHTML = "13º Propor. ...: R$: " + valor_decimo.toFixed(2)
 
-  document.getElementById("inss_base").innerHTML = 'BASE do Inss      R$: ' + baseInss.toFixed(2);
-  document.getElementById("inss_valor").innerHTML = 'Valor do Inss      R$: ' + valorInss.toFixed(2);
-  document.getElementById("irrf_base").innerHTML = 'BASE do IR      R$: ' + baseIR.toFixed(2);
-  document.getElementById("irrf_valor").innerHTML = 'Valor do IR      R$: ' + IR.toFixed(2);
-
+  document.getElementById("inssBase").innerHTML = 'BASE do Inss      R$: ' + baseInss.toFixed(2);
+  document.getElementById("inssValor").innerHTML = 'Valor do Inss      R$: ' + valorInss.toFixed(2);
+  document.getElementById("irrfBase").innerHTML = 'BASE do IR      R$: ' + baseIR.toFixed(2);
+  document.getElementById("irrfValor").innerHTML = 'Valor do IR      R$: ' + valor_IR.toFixed(2);
 
 }
-
