d67f8f0b56ac287b94e8e5188cdd587b1865d396
Guerreiro015 <antoniolucasluana@gmail.com>
2024-02-10 04:19:08 -0300

    atualizar 1002

--- calculoRescisao.js
+++ calculoRescisao.js
@@ -383,7 +383,7 @@ function calcularEncargos() {
   //---------------------------------------------------------
 
   total_proventos = sal_trabalhado + valor_do_adicional + valNot + valHe + aviso_previo + avisoProjetado_valor + valor_decimo + valor_ferias_vencidas + valor_ferias_propor + ferias_propor_terco + ferias_vencidas_terco + decimo_aviso + valor_medias13 + valor_medias_ferias + valor_medias_ferias_terco
-  total_descontos = valor_faltas + valor_atrasos + valor_dsr + valor_medico + valor_odonto + valor_sindicato + valor_VA + valor_VT + valor_IR + valorInss
+  total_descontos = valor_faltas + valor_atrasos + valor_dsr + valor_medico + valor_odonto + valor_sindicato + valor_VA + valor_VT + valor_IR + valorInss+valorinss13
 
   valor_rescisao = total_proventos - total_descontos
   //---------------------------------------------------------
